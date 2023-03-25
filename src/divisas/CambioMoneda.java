6958c2a20ce33fb249f8ab92a7d1c88f1cdb6d29
Ruben Chaparro <marte23122010@gmail.com>
2023-03-25 17:15:28 -0500

    Metodos de cambio de moneda y creacion de la clase principal

---
+++ src/divisas/CambioMoneda.java
@@ -0,0 +1,14 @@
+package divisas;
+
+public class CambioMoneda {
+
+
+    public CambioMoneda() {
+    }
+
+    public double aMonedaExtranjera(double divisaorigen, double divisaCambio) { return (divisaorigen / divisaCambio); }
+
+    public double aMonedaLocal( double divisa1, double divisa2) {
+        return divisa1 * divisa2;
+    }
+}
--- src/divisas/ClasePrincipal.java
+++ src/divisas/ClasePrincipal.java
@@ -1,4 +1,23 @@
 package divisas;
 
+import java.text.DecimalFormat;
+
 public class ClasePrincipal {
+
+    public static void main(String[] args) {
+
+        double divisaProporcionada = 100000000;
+        Divisas divisas = Divisas.DOLLAR;
+        CambioMoneda aMonedaExtranjera = new CambioMoneda();
+        CambioMoneda aMonedaLocal = new CambioMoneda();
+        DecimalFormat format = new DecimalFormat("#.00");
+
+        double aExtranjera = aMonedaExtranjera.aMonedaExtranjera(divisaProporcionada,divisas.getCambio());
+        double aLocal = aMonedaLocal.aMonedaLocal(divisaProporcionada, divisas.getCambio());
+        System.out.println(format.format(aLocal));
+        System.out.println(format.format(aExtranjera));
+
+
+
+    }
 }
